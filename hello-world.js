df02e3c8956d2d7334663f8772c76c17e8f98116
pallavi alse <pallavi1292@gmail.com>
2022-05-09 08:18:40 -0700

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,9 +1,3 @@
 console.log('Hello World');
-app.post("/records", (request, response) => {
-  const data = request.body;
-  const query = `SELECT * FROM health_records WHERE id = (${data.id})`;
-  connection.query(query, (err, rows) => {
-    if(err) throw err;
-    response.json({data:rows});
-  });
-});
+txtUserId = getRequestString("UserId");
+txtSQL = "SELECT * FROM Users WHERE UserId = " + txtUserId;
